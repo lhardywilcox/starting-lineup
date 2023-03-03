96d8dc3bb00c8ebe6413cc7bef0b34afc6258781
LHardyWilcox <motacycaryda@mac.com>
2023-03-03 13:51:36 -0800

    directory and code changes

---
+++ index.js
@@ -0,0 +1,7 @@
+const Employee = require('./Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+
+const questions = 
